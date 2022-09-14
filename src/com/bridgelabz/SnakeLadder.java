65407a8e6588427882f899c4f144c3a1424d8330
Rampravesh25 <rampraveshmallah@gmail.com>
2022-09-14 23:12:43 +0530

    Added: Rolling the dice for SnakeAndLadder

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -1,9 +1,15 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator");
 
         System.out.println("The player starts from position zero");
+
+        Random rnum = new Random();
+        int diceRoll = rnum.nextInt(6)+1;
+        System.out.println("Number on dice is :"+diceRoll);
     }
 }
