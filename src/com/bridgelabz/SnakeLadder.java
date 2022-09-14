2f705c4e276fc8a501a237adcfdcff2a94129720
Rampravesh25 <rampraveshmallah@gmail.com>
2022-09-14 23:15:09 +0530

    Added: Option for SnakeAndLadder

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -9,7 +9,18 @@ public class SnakeLadder {
         System.out.println("The player starts from position zero");
 
         Random rnum = new Random();
-        int diceRoll = rnum.nextInt(6)+1;
-        System.out.println("Number on dice is :"+diceRoll);
+        int diceRoll = rnum.nextInt(6) + 1;
+
+        int options = rnum.nextInt(3);
+        switch (options) {
+            case 0:
+                System.out.println("Player will stay on the position");
+                break;
+            case 1:
+                System.out.println("Player moves ahead with : " + diceRoll + " number");
+                break;
+            default:
+                System.out.println("Player moves behind : " + diceRoll + " number");
+        }
     }
 }
